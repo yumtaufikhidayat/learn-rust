28f23b954e9c1f91285ca51027635d640fc7e842
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2026-04-10 18:54:52 +0700

    Implement basic arithmetic and greeting functions in main.rs

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
+    let result = add(5, 3);
+    println!("The sum is: {}", result);
+
+    let nama_user = "Taufik"; // Ini tipenya &str
+    let message = hello(nama_user);
+    println!("{}", message);
+
+    let message_to_show = "This is a message to show.";
+    show_message(message_to_show);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn hello(name: &str) -> String {
+    format!("Hello {}, welcome to the world", name)
+}
+
+fn show_message(message: &str) {
+    println!("{}", message);
 }
