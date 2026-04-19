6c82425ebac797911da7ad0cf1522c13adb406b2
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2026-04-19 13:10:08 +0700

    Add some enum types and struct use cases

--- hello-rust/src/enum_ip_address_kind.rs
+++ hello-rust/src/enum_ip_address_kind.rs
@@ -1,3 +1,24 @@
 pub enum IpAddressKind {
     V4, V6
 }
+
+pub struct IpAddress {
+    pub kind: IpAddressKind,
+    pub address: String,
+}
+
+pub enum IpAddresses {
+    V4(String),
+    V6(String),
+}
+
+pub enum IpAddressCustom {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+pub enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
